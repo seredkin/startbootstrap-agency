7f017f6d06a2c9d47f489cd454821d0bf8a3c5e3
seredkin <anton.seredkin@gmail.com>
2016-12-16 16:32:59 +0300

    images, port 8888

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8888;
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(port, function(){
-    console.log('Server running on 8080...');
+    console.log('Server running on 8888...');
 });
